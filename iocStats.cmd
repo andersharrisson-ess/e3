0a0ef7cd5b5363af0af0583f8164b6776fc5f848
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-04-14 20:25:08 +0200

    update to match E3+

--- iocStats.cmd
+++ iocStats.cmd
@@ -1,10 +1,8 @@
-require require, 2.5.4
-require iocStats, 1856ef5
+require iocStats,1856ef5
 
 epicsEnvSet("PREF", "E3Test")
 epicsEnvSet("IOCST", "IocStat")
 epicsEnvSet("IOC",  ${PREF})
 
-#dbLoadRecords("moduleversion.db", "IOC=${IOC}")
 dbLoadRecords("iocAdminSoft.db","IOC=${PREF}:${IOCST}")
 dbl > "${IOC}_PVs.list"
